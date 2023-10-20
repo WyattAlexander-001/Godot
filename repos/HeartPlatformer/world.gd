a29dfed025b8fd2653f7a503073df1a0413c6f6d
Wyatt B <wyattbushman117@gmail.com>
2023-10-19 22:21:12 -0400

    added a new game learned alot

---
+++ repos/HeartPlatformer/world.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+@onready var collision_polygon_2d = $StaticBody2D/CollisionPolygon2D
+@onready var polygon_2d = $StaticBody2D/Polygon2D
+
+func _ready():
+	polygon_2d.polygon = collision_polygon_2d.polygon
