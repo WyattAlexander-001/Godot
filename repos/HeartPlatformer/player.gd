94458a6aca1321b7e792dfc46818351745d0142c
Wyatt B <wyattbushman117@gmail.com>
2023-10-19 23:53:58 -0400

    Got rid of comments made better functions

--- repos/HeartPlatformer/player.gd
+++ repos/HeartPlatformer/player.gd
@@ -1,28 +1,52 @@
 extends CharacterBody2D
 
-
+const ACCELERATION = 800
+const FRICTION = 1000
 const SPEED = 100.0
 const JUMP_VELOCITY = -300.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 
 func _physics_process(delta):
-	# Add the gravity.
+	var input_axis = Input.get_axis("a_left", "d_right")
+	update_animations(input_axis)
+	apply_gravity(delta)
+	handle_jump()
+	handle_acceleration(input_axis,delta)
+	apply_friction(input_axis,delta)
+	move_and_slide()
+	
+func apply_gravity(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+		
+func handle_jump():
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_accept"):
+			velocity.y = JUMP_VELOCITY
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+		if Input.is_action_just_released("ui_accept") and velocity.y < JUMP_VELOCITY / 2:
+			velocity.y = JUMP_VELOCITY / 2
+			
+func apply_friction(input_axis, delta):
+	if input_axis == 0:
+		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
+		
+func handle_acceleration(input_axis, delta):
+	if input_axis != 0:
+		velocity.x = move_toward(velocity.x, SPEED * input_axis, ACCELERATION * delta)
+		
+func update_animations(input_axis):
+	if input_axis != 0:
+		animated_sprite_2d.flip_h = (input_axis <0)
+		animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("idle")
+	if not is_on_floor():
+		animated_sprite_2d.play("jump")
+	
+	
+	
--- repos/HeartPlatformer/world.gd
+++ repos/HeartPlatformer/world.gd
@@ -4,4 +4,5 @@ extends Node2D
 @onready var polygon_2d = $StaticBody2D/Polygon2D
 
 func _ready():
+	RenderingServer.set_default_clear_color(Color.BLACK)
 	polygon_2d.polygon = collision_polygon_2d.polygon
