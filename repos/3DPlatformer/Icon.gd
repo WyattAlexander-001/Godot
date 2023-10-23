cc7e20e1405401860aa42c139b8f5451bc9e2b41
Wyatt B <wyattbushman117@gmail.com>
2023-10-23 02:49:59 -0400

    just made moving into a method

--- repos/3DPlatformer/Icon.gd
+++ repos/3DPlatformer/Icon.gd
@@ -14,5 +14,18 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame. 60fps
 func _process(delta):
-	rotate_y(0.1)
-#	rotate_z(0.1)
+	check_Inputs()
+
+
+func check_Inputs():
+	if Input.is_action_pressed("ui_left"):
+		rotate_y(deg_to_rad(-SPEED))
+		print(minion() + " LEFT")
+	elif Input.is_action_pressed("ui_right"):
+		rotate_y(deg_to_rad(SPEED))	
+		print(minion() + " RIGHT")
+		
+#	rotate_y(0.1)
+#	rotate_z(deg_to_rad(SPEED)
+func minion():
+	return "I'm a minion! Hello WORLD!"
--- repos/HeartPlatformer/player.gd
+++ repos/HeartPlatformer/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+
 const ACCELERATION = 800
 const FRICTION = 1000
 const SPEED = 100.0
@@ -8,10 +9,13 @@ const JUMP_VELOCITY = -300.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var coyote_jump_timer = $CoyoteJumpTimer
 
 
 func _physics_process(delta):
 	var input_axis = Input.get_axis("a_left", "d_right")
+	var was_on_floor = is_on_floor()
+	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0 # This is a horrible variable to be honest...
 	update_animations(input_axis)
 	apply_gravity(delta)
 	handle_jump()
@@ -19,16 +23,22 @@ func _physics_process(delta):
 	apply_friction(input_axis,delta)
 	move_and_slide()
 	
+	if just_left_ledge:
+		coyote_jump_timer.start()
+
+# ==============================================================================
+	
 func apply_gravity(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		
 func handle_jump():
-	if is_on_floor():
-		if Input.is_action_just_pressed("ui_accept"):
+	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
+		if Input.is_action_just_pressed("w_up"):
 			velocity.y = JUMP_VELOCITY
-	else:
-		if Input.is_action_just_released("ui_accept") and velocity.y < JUMP_VELOCITY / 2:
+			coyote_jump_timer.stop()  # Stop the timer once the jump is executed
+	if not is_on_floor():
+		if Input.is_action_just_released("w_up") and velocity.y < JUMP_VELOCITY / 2:
 			velocity.y = JUMP_VELOCITY / 2
 			
 func apply_friction(input_axis, delta):
