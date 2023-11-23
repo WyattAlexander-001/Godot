125bd8b5b9a96c0c5f61d54cd36922e920543c67
Wyatt B <wyattbushman117@gmail.com>
2023-11-22 20:28:42 -0500

    created explosion and success particles

--- repos/Boost/Player.gd
+++ repos/Boost/Player.gd
@@ -7,6 +7,11 @@ extends RigidBody3D
 @onready var explosion_audio: AudioStreamPlayer = $ExplosionAudio # hold ctrl and click/drag
 @onready var success_audio: AudioStreamPlayer = $SuccessAudio
 @onready var rocket_audio: AudioStreamPlayer3D = $RocketAudio
+@onready var booster_particles: GPUParticles3D = $BoosterParticles
+@onready var right_booster_particles: GPUParticles3D = $RightBoosterParticles
+@onready var left_booster_particles: GPUParticles3D = $LeftBoosterParticles
+@onready var explosion_particles: GPUParticles3D = $ExplosionParticles
+@onready var success_particles: GPUParticles3D = $SuccessParticles
 
 
 var is_transitioning: bool = false
@@ -24,7 +29,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
-		print("Spacebar pressed")
+		print("Spacebar pressed boost INPUT")
+		booster_particles.emitting = true
 		if rocket_audio.playing == false:
 			rocket_audio.play()
 		count+=1
@@ -32,12 +38,21 @@ func _process(delta: float) -> void:
 		apply_central_force(basis.y * delta * thrust)
 	else:
 		rocket_audio.stop()
+		booster_particles.emitting = false
+		
 		
 	if Input.is_action_pressed("rotate_left"):
 		apply_torque(Vector3(0.0,0.0,torque) * delta)
+		right_booster_particles.emitting = true
+	else:
+		right_booster_particles.emitting = false
+	
 	if Input.is_action_pressed("rotate_right"):
 		#rotate_z(-delta * 2)
 		apply_torque(Vector3(0.0,0.0,-torque) * delta)
+		left_booster_particles.emitting = true
+	else:
+		left_booster_particles.emitting = false
 
 
 # Player Body Touching A node with special GROUP NAME
@@ -53,6 +68,7 @@ func _on_body_entered(body: Node) -> void:
 
 func crash_sequence() -> void:
 	print("YOU CRASHED! We Wait 1 Second And Then Reload")
+	explosion_particles.emitting = true
 	explosion_audio.play()
 	var tween = create_tween()
 	set_process(false) #disables process function so we can't control ship
@@ -62,6 +78,7 @@ func crash_sequence() -> void:
 
 func complete_level(next_level_file: String) -> void:
 	print("You WIN!!!")
+	success_particles.emitting = true
 	success_audio.play()
 	var tween = create_tween()
 	set_process(false) #disables process function so we can't control ship
