9c78d8814781e985d2c89b3473d41e2e222cadfd
Wyatt B <wyattbushman117@gmail.com>
2024-01-05 09:53:49 -0500

    set a fall factor and max height to jump
    Also added platforms

--- repos/FPSBasics/player/player.gd
+++ repos/FPSBasics/player/player.gd
@@ -2,7 +2,8 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+@export var jump_height: float = 1.0
+@export var fall_multiplier: float = 2.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -16,11 +17,14 @@ func _physics_process(delta: float) -> void:
 	handle_camera_rotation()
 	# Add the gravity (downward velocity when player is NOT on floor).
 	if not is_on_floor():
-		velocity.y -= gravity * delta
+		if velocity.y >= 0:
+			velocity.y -= gravity * delta
+		else:
+			velocity.y -= gravity * delta * fall_multiplier
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = sqrt(jump_height * 2.0 * gravity) 
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
