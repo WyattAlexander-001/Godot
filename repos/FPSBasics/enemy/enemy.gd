018463215307289f821403a92f0e6172bd6612a6
Wyatt B <wyattbushman117@gmail.com>
2024-01-07 20:09:00 -0500

    added ai movement
    ai will chase player once in its 10m zone

--- repos/FPSBasics/enemy/enemy.gd
+++ repos/FPSBasics/enemy/enemy.gd
@@ -8,27 +8,29 @@ const JUMP_VELOCITY = 4.5
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var navigation_agent_3d = $NavigationAgent3D
 var player
+var provoked := false
+var aggro_range := 10.0
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
 
+func _process(_delta: float) -> void: #_delta means optional parameter
+	if provoked == true: # Could also use if provoked: since true is implied
+		navigation_agent_3d.target_position = player.global_position
+
 func _physics_process(delta: float) -> void:
-	
-	navigation_agent_3d.target_position = player.global_position
 	var next_position = navigation_agent_3d.get_next_path_position()
 	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = global_position.direction_to(next_position)
+	var distance = global_position.distance_to(player.global_position)
+	
+	if distance <= aggro_range:
+		provoked = true
+	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -36,4 +38,4 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
-#	move_and_slide()
+	move_and_slide()
