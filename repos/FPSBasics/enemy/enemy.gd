0685b3c3de19cef7b243bd1875f701c95be00708
Wyatt B <wyattbushman117@gmail.com>
2024-01-07 19:27:55 -0500

    added platforms and ai routing

---
+++ repos/FPSBasics/enemy/enemy.gd
@@ -0,0 +1,39 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+@onready var navigation_agent_3d = $NavigationAgent3D
+var player
+
+func _ready() -> void:
+	player = get_tree().get_first_node_in_group("player")
+
+func _physics_process(delta: float) -> void:
+	
+	navigation_agent_3d.target_position = player.global_position
+	var next_position = navigation_agent_3d.get_next_path_position()
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+#	move_and_slide()
