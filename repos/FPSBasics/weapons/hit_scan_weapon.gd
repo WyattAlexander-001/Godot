33a9d0831b43cee4cb155961ca52f29d4822f80f
Wyatt B <wyattbushman117@gmail.com>
2024-01-09 10:40:43 -0500

    finished recoil animation

--- repos/FPSBasics/weapons/hit_scan_weapon.gd
+++ repos/FPSBasics/weapons/hit_scan_weapon.gd
@@ -5,14 +5,19 @@ extends Node3D
 @export var weapon_mesh : Node3D
 
 @onready var cool_down_timer = $CoolDownTimer
+@onready var weapon_position: Vector3 = weapon_mesh.position
+@onready var ray_cast_3d = $RayCast3D
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("fire"):
 		if cool_down_timer.is_stopped():
 			shoot()
+	weapon_mesh.position = weapon_mesh.position.lerp(weapon_position,delta * 10.0)
 
 func shoot() -> void:
 	cool_down_timer.start(1.0/fire_rate)
-	print("weapon fired")
+	printt("weapon fired",ray_cast_3d.get_collider())
 	weapon_mesh.position.z += recoil
+
