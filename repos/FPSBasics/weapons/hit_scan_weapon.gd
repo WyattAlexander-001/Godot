0d3e42a70a58c6ebb255615addd46ea4369d6324
Wyatt B <wyattbushman117@gmail.com>
2024-01-09 08:37:28 -0500

    added recoil
    need to reset animation, but it works

--- repos/FPSBasics/weapons/hit_scan_weapon.gd
+++ repos/FPSBasics/weapons/hit_scan_weapon.gd
@@ -1,16 +1,18 @@
 extends Node3D
 
 @export var fire_rate := 14.0
+@export var recoil := 0.05
+@export var weapon_mesh : Node3D
+
 @onready var cool_down_timer = $CoolDownTimer
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(delta: float) -> void:
 	if Input.is_action_pressed("fire"):
 		if cool_down_timer.is_stopped():
-			cool_down_timer.start(1.0/fire_rate)
-			print("weapon fired")
+			shoot()
+
+func shoot() -> void:
+	cool_down_timer.start(1.0/fire_rate)
+	print("weapon fired")
+	weapon_mesh.position.z += recoil
