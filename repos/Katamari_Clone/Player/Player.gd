36e057c545be4dca2c420a77351a670ca0079139
WyattAlexander-001 <wyattbushman117@gmail.com>
2024-04-04 01:21:01 -0400

    added cursor visibility using esc and leftclick also added more inputs

---
+++ repos/Katamari_Clone/Player/Player.gd
@@ -0,0 +1,26 @@
+extends RigidBody3D
+
+@export var ballSpeed := 1200
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED) # Hides Mouse On Start
+
+	
+func _process(delta: float) -> void:
+	var input := Vector3.ZERO
+	input.x = Input.get_axis("move_left","move_right")
+	input.z = Input.get_axis("move_forward","move_back")
+	apply_central_force(input * ballSpeed * delta)
+	_toggleCursor()
+
+func _toggleCursor():
+	if Input.is_action_just_pressed("ui_cancel"): # For hitting escape key to toggle mouse cursor
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+			
+	elif Input.is_action_just_pressed("left_click") && Input.get_mouse_mode() != Input.MOUSE_MODE_CAPTURED:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED) # Hides cursor upon left click
+
+	
